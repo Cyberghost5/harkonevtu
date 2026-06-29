9669e3cb3fcfeb76bdf08770a1351342adccaea1
Adebisi Covenant <adebisicovenant01@gmail.com>
2026-06-30 00:44:08 +0100

    new Uopdate

--- app/Http/Controllers/AirtimeController.php
+++ app/Http/Controllers/AirtimeController.php
@@ -336,7 +336,7 @@ class AirtimeController extends Controller
     {
         $endpoint   = config('services.autopilot.base_url') . '/airtime';
         $payload    = [
-            'networkId' => $network->id,
+            'networkId' => (string) $network->id,
             'amount' => (string) $amount,
             'phone' => $phone,
             'airtimeType' => 'VTU',
