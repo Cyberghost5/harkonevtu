ef85833e1567544ebb45990a67cc92ca67b8fc1a
Adebisi Covenant <adebisicovenant01@gmail.com>
2026-09-02 21:14:24 +0100

    Fix missing Illuminate\Support\Str import in UserController.php for agent upgrade endpoint

--- app/Http/Controllers/Api/v1/User/UserController.php
+++ app/Http/Controllers/Api/v1/User/UserController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
